3be90985a8dfd65e174e9f76c85fe433d6abd623
aid16 <aid16@ic.ac.uk>
2017-02-04 19:22:56 +0000

    ICHack

--- app/src/main/java/com/example/radu/ichack/MainActivity.java
+++ app/src/main/java/com/example/radu/ichack/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity
     setSupportActionBar(toolbar);
 
 
+
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
         this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
